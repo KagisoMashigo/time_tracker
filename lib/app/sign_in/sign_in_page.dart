10a23edf7d7c85d533b1f195527d7d11717c7d7a
Masterkakes <kagiso.k.mashigo@gmail.com>
2021-04-28 10:26:10 -0400

    begin sign_in page

---
+++ lib/app/sign_in/sign_in_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class SignInPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(
+          child: Text('Time Tracker'),
+        ),
+        elevation: 4.0,
+      ),
+    );
+  }
+}
+
