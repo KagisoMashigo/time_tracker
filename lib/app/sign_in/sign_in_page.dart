5be5109016081ac7b1ed2d03d5d8a1685a063dcb
Masterkakes <kagiso.k.mashigo@gmail.com>
2021-04-28 16:59:01 -0400

    add images to buttons

--- lib/app/sign_in/sign_in_page.dart
+++ lib/app/sign_in/sign_in_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:time_tracker_flutter_course/app/sign_in/sign_in_button.dart';
 import 'package:time_tracker_flutter_course/common_widgets/custom_raised_button.dart';
@@ -25,12 +26,29 @@ class SignInPage extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
           Text(
-            'Sign in',
+            'Sign In',
             textAlign: TextAlign.center,
             style: TextStyle(
               fontSize: 32.0,
               fontWeight: FontWeight.w600,
             ),
+          ),
+          SizedBox(height: 48.0),
+          // The below is a great hack when spacing two children evenly
+          CustomRaisedButton(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Image.asset('images/google-logo.png'),
+                Text('Sign in with Google'),
+                Opacity(
+                    opacity: 0.0,
+                    child: Image.asset('images/google-logo.png'),
+                ),
+              ],
+            ),
+            color: Colors.white,
+            onPressed: () {},
           ),
           SizedBox(height: 8.0),
           SignInButton(
@@ -38,7 +56,34 @@ class SignInPage extends StatelessWidget {
             textColor: Colors.black87,
             color: Colors.white,
             onPressed: () {},
-          )
+          ),
+          SizedBox(height: 8.0),
+          SignInButton(
+            text: 'Sign in with Facebook',
+            textColor: Colors.white,
+            color: Color(0xFF334D92),
+            onPressed: () {},
+          ),
+          SizedBox(height: 8.0),
+          SignInButton(
+            text: 'Sign in with email',
+            textColor: Colors.white,
+            color: Colors.teal[700],
+            onPressed: () {},
+          ),
+          SizedBox(height: 8.0),
+          Text(
+              'or',
+            style: TextStyle(fontSize: 14.0, color: Colors.black87),
+            textAlign: TextAlign.center,
+          ),
+          SizedBox(height: 8.0),
+          SignInButton(
+            text: 'Go anonymous',
+            textColor: Colors.black,
+            color: Colors.lime[300],
+            onPressed: () {},
+          ),
         ],
       ),
     );
