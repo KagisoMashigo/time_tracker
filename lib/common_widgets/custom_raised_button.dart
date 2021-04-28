5b124eb2aa03e986628a9d4ece0272ea8e463d30
Masterkakes <kagiso.k.mashigo@gmail.com>
2021-04-28 15:46:30 -0400

    add sign_in button

--- lib/common_widgets/custom_raised_button.dart
+++ lib/common_widgets/custom_raised_button.dart
@@ -3,27 +3,32 @@ import 'package:flutter/material.dart';
 class CustomRaisedButton extends StatelessWidget {
   const CustomRaisedButton({
     this.color,
-    this.borderRadius,
+    this.borderRadius: 4.0,
     this.onPressed,
+    this.height: 50,
     this.child,
   });
 
   final Widget child;
   final Color color;
   final double borderRadius;
+  final double height;
   final VoidCallback onPressed;
 
   @override
   Widget build(BuildContext context) {
-    return RaisedButton(
-      child: child,
-      color: color,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.all(
-          Radius.circular(borderRadius),
+    return SizedBox(
+      height: height,
+      child: RaisedButton(
+        child: child,
+        color: color,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(
+            Radius.circular(borderRadius),
+          ),
         ),
+        onPressed: onPressed,
       ),
-      onPressed: onPressed,
     );
   }
 }
