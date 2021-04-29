77338e5121e487049ce55ab85dcdf241f4a231fe
Masterkakes <kagiso.k.mashigo@gmail.com>
2021-04-29 17:06:19 -0400

    being landing page

--- lib/app/landing_page.dart
+++ lib/app/landing_page.dart
@@ -1,5 +1,5 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:time_tracker_flutter_course/app/sign_in/sign_in_button.dart';
 import 'package:time_tracker_flutter_course/app/sign_in/sign_in_page.dart';
 
 class LandingPage extends StatefulWidget {
@@ -11,11 +11,17 @@ class _LandingPageState extends State<LandingPage> {
 
   User _user;
 
+  void _updateUser(User user) {
+    print('user id: ${user.uid}');
+  }
+
   @override
   Widget build(BuildContext context) {
 
     if (_user == null) {
-      return SignInPage();
+      return SignInPage(
+        onSignIn: (user) => _updateUser(user),
+      );
     }
     return Container(); // placeholder
   }
