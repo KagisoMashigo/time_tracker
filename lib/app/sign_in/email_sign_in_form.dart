cd4d7d5c3e036e6907d199d753d8403040acb712
KagisoMashigo <kagiso.k.mashigo@gmail.com>
2021-05-06 11:14:08 -0400

    begin email sign in

--- lib/app/sign_in/email_sign_in_form.dart
+++ lib/app/sign_in/email_sign_in_form.dart
@@ -1,24 +1,86 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:time_tracker_flutter_course/common_widgets/form_submit_button.dart';
 
-class EmailSIgnInForm extends StatelessWidget {
+enum EmailSignInFormType { signIn, register }
+
+class EmailSIgnInForm extends StatefulWidget {
   const EmailSIgnInForm({Key key}) : super(key: key);
 
+  @override
+  _EmailSIgnInFormState createState() => _EmailSIgnInFormState();
+}
+
+class _EmailSIgnInFormState extends State<EmailSIgnInForm> {
+  // const EmailSIgnInForm({Key key}) : super(key: key);
+
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  EmailSignInFormType _formType = EmailSignInFormType.signIn;
+
+  void _submit() {
+    print(
+        'Email: ${_emailController.text}, password: ${_passwordController.text}');
+  }
+
+  void _toggleText() {
+    setState(() {
+      _formType = _formType == EmailSignInFormType.signIn ?
+          EmailSignInFormType.register : EmailSignInFormType.signIn;
+    });
+  }
+
   List<Widget> _buildChildren() {
+    final primaryText = _formType == EmailSignInFormType.signIn
+        ? 'Sign In'
+        : 'Create an Account';
+    final secondaryText = _formType == EmailSignInFormType.signIn
+        ? 'Need an Account? Register'
+        : 'Have an Account? Sign In';
     return [
       TextField(
+        controller: _emailController,
+        decoration:
+            InputDecoration(labelText: 'Email', hintText: 'time@tracker.com'),
+      ),
+      SizedBox(
+        height: 8.0,
+      ),
+      TextField(
+        controller: _passwordController,
         decoration: InputDecoration(
-          labelText: 'Email',
-          hintText: 'time@tracker.com'
+          labelText: 'Password',
+          // hintText: '12345 - jk',
         ),
+        obscureText: true,
+      ),
+      SizedBox(
+        height: 8.0,
+      ),
+      FormSubmitButton(
+        text: primaryText,
+        onPressed: _submit,
+      ),
+      SizedBox(
+        height: 8.0,
+      ),
+      FlatButton(
+        child: Text(secondaryText),
+        onPressed: _toggleText ,
       ),
     ];
   }
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: _buildChildren(),
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: _buildChildren(),
+      ),
     );
   }
 }
