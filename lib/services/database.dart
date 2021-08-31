0fa170c6f6b63f63d3ccbe293249c90bf5f39a01
KagisoMashigo <kagiso.k.mashigo@gmail.com>
2021-08-31 14:32:34 -0400

    add delete fucntions - UI next

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -8,7 +8,7 @@ abstract class Database {
 
   Stream<List<Job>> jobsStream();
 
-  // Future<void> deleteJob(Job job);
+  Future<void> deleteJob(Job job);
 
   // Future<void> deleteEntry(Job job);
 
@@ -26,11 +26,16 @@ class FirestoreDatabase implements Database {
   // Private contructor that only allows one instance of the class to be created
   final _service = FirestoreService.instance;
 
+  @override
   Future<void> setJob(Job job) async => await _service.setData(
         path: APIPath.job(uid, job.id),
         data: job.toMap(),
       );
-
+  @override
+  Future<void> deleteJob(Job job) async => await _service.deleteData(
+        path: APIPath.job(uid, job.id),
+      );
+  @override
   Stream<List<Job>> jobsStream() => _service.collectionStream(
         path: APIPath.jobs(uid),
         builder: (data, documentId) => Job.fromMap(data, documentId),
