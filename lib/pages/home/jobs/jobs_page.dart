ff8d87845cc71f2c6edd7d5b5d672ba624cf2159
KagisoMashigo <kagiso.k.mashigo@gmail.com>
2021-08-31 15:57:28 -0400

    add swipe delete

--- lib/pages/home/jobs/jobs_page.dart
+++ lib/pages/home/jobs/jobs_page.dart
@@ -1,8 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:time_tracker_flutter_course/components/alert_dialogs/show_alert_dialog.dart';
 import 'package:time_tracker_flutter_course/components/builders/list_items_builder.dart';
+import 'package:time_tracker_flutter_course/components/errors/show_exception_alert_dialog.dart';
 import 'package:time_tracker_flutter_course/components/job_list_tile.dart';
 import 'package:time_tracker_flutter_course/core/data/models/job.dart';
 import 'package:time_tracker_flutter_course/pages/home/jobs/edit_job_page.dart';
@@ -32,19 +34,17 @@ class JobsPage extends StatelessWidget {
     }
   }
 
-  Widget _buildContents(BuildContext context) {
-    final database = Provider.of<Database>(context, listen: false);
-    return StreamBuilder<List<Job>>(
-      stream: database.jobsStream(),
-      builder: (context, snapshot) {
-        return ListItemsBuilder<Job>(
-            snapshot: snapshot,
-            itemBuilder: (context, job) => JobsListTile(
-                  job: job,
-                  onTap: () => EditJobPage.show(context, job: job),
-                ));
-      },
-    );
+  Future<void> _delete(BuildContext context, Job job) async {
+    try {
+      final database = Provider.of<Database>(context, listen: false);
+      await database.deleteJob(job);
+    } on FirebaseException catch (e) {
+      showExceptionAlertDialog(
+        context,
+        title: "'Operation Failed",
+        exception: e,
+      );
+    }
   }
 
   @override
@@ -70,6 +70,27 @@ class JobsPage extends StatelessWidget {
         onPressed: () => EditJobPage.show(context),
         child: Icon(Icons.add),
       ),
+    );
+  }
+
+  Widget _buildContents(BuildContext context) {
+    final database = Provider.of<Database>(context, listen: false);
+    return StreamBuilder<List<Job>>(
+      stream: database.jobsStream(),
+      builder: (context, snapshot) {
+        return ListItemsBuilder<Job>(
+            snapshot: snapshot,
+            itemBuilder: (context, job) => Dismissible(
+                  key: Key('job-${job.id}'),
+                  background: Container(color: Colors.red),
+                  onDismissed: (direction) => _delete(context, job),
+                  direction: DismissDirection.endToStart,
+                  child: JobsListTile(
+                    job: job,
+                    onTap: () => EditJobPage.show(context, job: job),
+                  ),
+                ));
+      },
     );
   }
 }
