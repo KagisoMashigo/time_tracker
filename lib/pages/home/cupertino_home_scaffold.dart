6660231bf175fbf2caab74b74e64bc79ac99fa48
KagisoMashigo <kagiso.k.mashigo@gmail.com>
2021-09-01 15:54:40 -0400

    begin bottom nav

---
+++ lib/pages/home/cupertino_home_scaffold.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:time_tracker_flutter_course/pages/home/tab_item.dart';
+
+class CupertinoHomeScaffold extends StatelessWidget {
+  const CupertinoHomeScaffold({
+    Key key,
+    @required this.currentTab,
+    @required this.onSelectedTab,
+  }) : super(key: key);
+
+  final TabItem currentTab;
+  final ValueChanged<TabItem> onSelectedTab;
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoTabScaffold(
+        tabBar: CupertinoTabBar(
+      items: [],
+      onTap: (index) => onSelectedTab(TabItem.values[index]),
+    ));
+  }
+}
